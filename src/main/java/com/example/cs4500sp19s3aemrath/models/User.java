67fe6731e6c620cccc22b304d4f7a9f336ea8d7e
Abigail Emrath <aemrath@Abigails-MacBook-Air.local>
2019-01-23 19:33:05 -0500

    User service and user page

--- src/main/java/com/example/cs4500sp19s3aemrath/models/User.java
+++ src/main/java/com/example/cs4500sp19s3aemrath/models/User.java
@@ -1,5 +1,57 @@
 package com.example.cs4500sp19s3aemrath.models;
+import java.util.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 public class User {
-
-}
+	public Integer id;
+	public String username;
+	public String password;
+	public String firstName;
+	public String lastName;
+	
+	public User(int id, String username, String password, String firstName, String lastName) {
+		
+	this.id = id;
+	this.username = username;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	}
+	// provide a public set and get method for each of the class variables above
+	// make sure to use proper naming conventions for Java methods, e.g.,
+	// methods should be camelcase. Here's an example using the firstName variable:
+ 	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+ }
